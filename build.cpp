543994ba0cbf5da7595d13007c152d852650e4b4
YvanCywan <c1yvan@outlook.com>
2026-01-13 18:18:43 +0000

    feat: dependencies

--- build.cpp
+++ build.cpp
@@ -1,4 +1,4 @@
-#include "src/anvil/api.hpp"
+#include "anvil/api.hpp"
 #include <filesystem>
 #include <iostream>
 
@@ -8,9 +8,11 @@ void configure(anvil::Project& project) {
     project.name = "Anvil";
 
     project.add_executable("anvil", [](anvil::CppApplication& app) {
+        // Anvil needs access to "src" because it includes "anvil/api.hpp" which is in "src/anvil"
         app.add_include("src");
     });
 
     project.add_test("anvil_tests", [](anvil::CppApplication& app) {
+        // Configuration moved to anvil::Project::add_test
     });
 }
