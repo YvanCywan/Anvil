1738973e759ca5bf82f70c7061fa0d5bfb1c8dfd
YvanCywan <c1yvan@outlook.com>
2026-01-13 16:12:26 +0000

    feat: testing framework

--- build.cpp
+++ build.cpp
@@ -1,13 +1,5 @@
 #include "anvil/api.hpp"
 
-#ifndef ANVIL_API_V2
-// Legacy support for bootstrapping with Anvil 0.1.x
-void anvil::Project::add_executable(const std::string &name, std::function<void(CppApplication &)> config) {
-    application.name = name;
-    config(application);
-}
-#endif
-
 void configure(anvil::Project& project) {
     project.name = "Anvil";
 
@@ -21,5 +13,10 @@ void configure(anvil::Project& project) {
 #ifdef _WIN32
         app.add_link_flag("-static");
 #endif
+    });
+    project.add_test("anvil_tests", [](anvil::CppApplication& app) {
+        app.standard = anvil::CppStandard::CPP_20;
+        app.add_source("src/test/test_main.cpp");
+        app.add_include("src");
     });
 }
